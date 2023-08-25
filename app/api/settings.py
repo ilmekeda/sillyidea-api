892acfcfd72306324c3acf876fda5cbf0fb32f8e
Illy Mik <ilmekeda@gmail.com>
2023-08-24 21:55:00 -0400

    add migrations

--- app/api/settings.py
+++ app/api/settings.py
@@ -136,3 +136,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# use custom user model
+AUTH_USER_MODEL = 'core.User'
--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,90 @@
-from django.db import models  # noqa
+"""Database Models"""
+from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
+from django.core.mail import send_mail
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+def update_last_login(sender, user, **kwargs):
+    """
+    A signal receiver which updates the last_login date for
+    the user logging in.
+    """
+    user.last_login = timezone.now()
+    user.save(update_fields=['last_login'])
+
+
+class UserManager(BaseUserManager):
+    """Manager for a user model"""
+
+    def create_user(self, email, password=None, **extra_fields):
+        """
+        Create, save, and return a new user.
+        NOTE: a password with none is for testing or user that cannot login
+        """
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """
+    User in this system using custom model with
+    admin-compliant permissions
+    """
+    email = models.EmailField(
+        _('email address'),
+        max_length=255,
+        unique=True,
+        error_messages={
+            'unique': _("A user with that email already exists."),
+        },
+    )
+    first_name = models.CharField(_('first name'), max_length=125)
+    last_name = models.CharField(_('last name'), max_length=125)
+    is_active = models.BooleanField(
+        default=True,
+        help_text=_(
+            'Designates whether this user should be treated as active. '
+            'Unselect this instead of deleting accounts.'
+        ),
+    )
+    is_staff = models.BooleanField(
+        _('staff status'),
+        default=False,
+        help_text=_('Designates whether the user can log into this admin site.'),
+    )
+    date_joined = models.DateTimeField(
+        _('date joined'),
+        default=timezone.now
+    )
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+
+    class Meta:
+        verbose_name = _('user')
+        verbose_name_plural = _('users')
+
+    def get_full_name(self):
+        """
+        Return the first_name plus the last_name, with a space in between.
+        """
+        full_name = '%s %s' % (self.first_name, self.last_name)
+        return full_name.strip()
+
+    def get_short_name(self):
+        """Return the short name for the user."""
+        return self.first_name
+
+    def email_user(self, subject, message, from_email=None, **kwargs):
+        """Send an email to this user."""
+        send_mail(subject, message, from_email, [self.email], **kwargs)
